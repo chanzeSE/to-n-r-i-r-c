c99452cc4028eae5e203521e7d76f7978fae6534
iw-aurora <trangcute04022007@gmail.com>
2026-06-12 22:28:03 +0700

    new2

--- bct.cpp
+++ bct.cpp
@@ -2,82 +2,98 @@
 #include <vector>
 #include <string>
 using namespace std;
-bool noilien(bool p, bool q){
-    return p && q;
-}
-bool keotheo(bool p, bool q){
-    return !p || q;
-}
-string center(string s, int w){
+
+string center(string s, int w) {
     int len = s.length();
-    int left = (w - len)/2;
+    int left = (w - len) / 2;
     int right = w - len - left;
     return string(left, ' ') + s + string(right, ' ');
 }
 
-void line(){
-    cout << "+-----+-----+-----+-------+----------+\n";
+void line(int n) {
+    for (int i = 0; i < n; i++)
+        cout << "+-----";
+    cout << "+------------+\n";
 }
-void hangDung(){
-    cout << "BANG CHAN TRI\n";
-    cout << "p\tq\tk\tGiaTri\n";
 
-    bool hangDung = true;
-
-    for (int i = 0; i <= 1; i++) {
-        for (int j = 0; j <= 1; j++) {
-        for (int k = 0; k<= 1; k++) {
-            bool p = (i == 1);
-            bool q = (j == 1);
-            bool r = (k ==1 );
-            bool ketQua = noilien(keotheo(p, q), r);
-            cout << i << "\t" << j << "\t" << k << "\t" << ketQua << endl;
-
-            if (!ketQua)
-                hangDung = false;
-        }
-    }
+bool phepVa(bool x, bool y) {
+    return x && y;
 }
-    cout << endl;
-    if (hangDung)
-       cout << "Bieu thuc la hang dung." << endl;
-    else
-        cout << "Bieu thuc khong phai hang dung." << endl;
+
+bool phepHoac(bool x, bool y) {
+    return x || y;
 }
-int main(){
-    int n = 3;
-    int rows = 1<<n; 
+
+bool phepPhuDinh(bool x) {
+    return !x;
+}
+
+bool phepKeoTheo(bool x, bool y) {
+    return phepHoac(phepPhuDinh(x), y);
+}
+
+bool phepTuongDuong(bool x, bool y) {
+    return phepKeoTheo(x, y) && phepKeoTheo(y, x);
+}
+
+// (p -> q) ^ p
+bool bieuThuc(bool p, bool q) {
+    return phepVa(phepKeoTheo(p, q), p);
+}
+
+int main() {
+
+    int n = 4;
+    int rows = 1 << n;
+
     vector<vector<bool>> matrix(n, vector<bool>(rows));
-    // sinh bảng chân trị
+
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < n; j++) {
             matrix[j][i] = (i >> (n - j - 1)) & 1;
         }
     }
 
-    line();
-    cout << "|"
-         << center("p",5) << "|"
-         << center("q",5) << "|"
-         << center("r",5) << "|"
-         << center("p->q",7) << "|"
-         << center("(p->q)^r",10) << "|\n";
-    line();
+    bool hangDung = true;
+
+    cout << "\nBANG CHAN TRI\n";
+
+    line(n);
+
+    cout << "|";
+    for (int i = 0; i < n; i++) {
+        string var(1, 'p' + i);
+        cout << center(var, 5) << "|";
+    }
+    cout << center("(p->q)^p", 12) << "|\n";
+
+    line(n);
 
     for (int i = rows - 1; i >= 0; i--) {
+
         bool p = matrix[0][i];
         bool q = matrix[1][i];
-        bool r = matrix[2][i];
 
-        cout << "|"
-             << center(to_string(p),5) << "|"
-             << center(to_string(q),5) << "|"
-             << center(to_string(r),5) << "|"
-             << center(to_string(keotheo(p,q)),7) << "|"
-             << center(to_string(noilien(keotheo(p,q), r)),10) << "|\n";
+        bool result = bieuThuc(p, q);
+
+        cout << "|";
+
+        for (int j = 0; j < n; j++) {
+            cout << center(to_string(matrix[j][i]), 5) << "|";
+        }
+
+        cout << center(to_string(result), 12) << "|\n";
+
+        if (!result)
+            hangDung = false;
     }
 
-    line();
-    hangDung();
+    line(n);
+
+    if (hangDung)
+        cout << "\nBieu thuc la hang dung.\n";
+    else
+        cout << "\nBieu thuc khong phai hang dung.\n";
+
     return 0;
 }
